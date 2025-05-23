8745db6ec6ad88feb826a000898f6b25a72809f0
erick.souza <ericksouza@insightlab.ufc.br>
2025-05-23 17:03:56 -0300

    ädding

--- Package.swift
+++ Package.swift
@@ -42,16 +42,15 @@ let package = Package(
             resources: [.process("ggml-metal.metal")],
             publicHeadersPath: "spm-headers",
             cSettings: [
-                .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
+                // Remova o -O3 e -DNDEBUG para deixar o Xcode controlar otimização
+                // .unsafeFlags(["-Wno-shorten-64-to-32"]), // mantenha se necessário
+                
                 .define("GGML_USE_ACCELERATE"),
+                // Se o -fno-objc-arc for necessário para os arquivos .m (ggml-metal.m), pode ser obrigatório manter:
                 .unsafeFlags(["-fno-objc-arc"]),
                 .define("GGML_USE_METAL")
-                // NOTE: NEW_LAPACK will required iOS version 16.4+
-                // We should consider add this in the future when we drop support for iOS 14
-                // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
-                // .define("ACCELERATE_NEW_LAPACK"),
-                // .define("ACCELERATE_LAPACK_ILP64")
             ],
+
             linkerSettings: [
                 .linkedFramework("Accelerate")
             ]
